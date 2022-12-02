51f1f4ebed2bed9078caa8e40b948cf2c5e46c2d
tanmayvaij <tanmayvaij22@gmail.com>
2022-12-02 18:42:05 +0530

    added intersection method in q6

--- arrays/question_06.cpp
+++ arrays/question_06.cpp
@@ -31,6 +31,22 @@ vector<int> doUnion(vector<int> v1, vector<int> v2) {
 
 }
 
+vector<int> doIntersection(vector<int> v1, vector<int> v2) {
+
+    vector<int> check(100001, 0);
+
+    vector<int> intersec_arr;
+
+    for ( int i = 0; i < v1.size(); i++ ) check[v1[i]]++;
+
+    for ( int i = 0; i < v2.size(); i++ ) check[v2[i]]++;
+
+    for ( int i = 0; i < check.size(); i++ ) if ( check[i] == 2 ) intersec_arr.push_back(i);   
+
+    return intersec_arr; 
+
+}
+
 int main () {
 
     vector<int> arr1 = { 1, 2, 3, 4, 5, 6 };
@@ -39,7 +55,13 @@ int main () {
 
     vector<int> union_arr = doUnion(arr1, arr2);
 
+    vector<int> intersec_arr = doIntersection(arr1, arr2);
+
+    cout << "Union of arrays:- ";
     printVector(union_arr);
+
+    cout << "Intersection of arrays:- ";
+    printVector(intersec_arr);
 
     return 0;
 
