29d488ae69b2be3d9cce9b5d82d908f19aa55ccc
tanmayvaij <tanmayvaij22@gmail.com>
2022-12-02 18:37:52 +0530

    added q6

---
+++ arrays/question_06.cpp
@@ -0,0 +1,46 @@
+/*
+
+
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printVector(vector<int> v) {
+
+    for ( int i = 0; i < v.size(); i++ ) cout << v[i] << " ";
+
+    cout << endl;
+
+}
+
+vector<int> doUnion(vector<int> v1, vector<int> v2) {
+
+    vector<int> check(100001, 0);
+
+    vector<int> union_arr;
+
+    for ( int i = 0; i < v1.size(); i++ ) check[v1[i]]++;
+
+    for ( int i = 0; i < v2.size(); i++ ) check[v2[i]]++;
+
+    for ( int i = 0; i < check.size(); i++ ) if ( check[i] > 0 ) union_arr.push_back(i);
+
+    return union_arr;
+
+}
+
+int main () {
+
+    vector<int> arr1 = { 1, 2, 3, 4, 5, 6 };
+
+    vector<int> arr2 = { 4, 5, 6, 7, 8, 9 };
+
+    vector<int> union_arr = doUnion(arr1, arr2);
+
+    printVector(union_arr);
+
+    return 0;
+
+}
